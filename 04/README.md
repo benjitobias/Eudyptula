e3fd01eb5ea677eb0a2daf4932283cf17c5c2313
benjitobias <benjitobias@gmail.com>
2017-04-19 22:33:49 +0300

    Added task 04

---
+++ 04/README.md
@@ -0,0 +1,2 @@
+I forgot to run checkpatch.pl and I also submitted the code instaed of a patch.
+
---
+++ 04/src/hello.c
@@ -0,0 +1,21 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BENJI");
+MODULE_DESCRIPTION("A simple Hello World module");
+
+static int __init hello_init(void)
+{
+    printk(KERN_DEBUG "hello: Hello world!\n");
+    return 0;
+}
+
+static void __exit hello_cleanup(void)
+{
+    printk(KERN_DEBUG "hello: Cleaning up module.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
