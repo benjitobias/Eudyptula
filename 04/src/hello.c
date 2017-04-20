716969f0c82839d4adc0e9f9e7a034853cf0148f
benjitobias@gmail.com <benjitobias@gmail.com>
2017-04-20 18:34:36 +0300

    Reset to original to create patch

--- 04/src/hello.c
+++ 04/src/hello.c
@@ -1,6 +1,6 @@
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
+#include <linux/module.h> // included for all kernel modules
+#include <linux/kernel.h> // included for KERN_DEBUG
+#include <linux/init.h> // included for __init and __exit macros
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("BENJI");
@@ -8,12 +8,18 @@ MODULE_DESCRIPTION("A simple Hello World module");
 
 static int __init hello_init(void)
 {
+    /*
+        Initialize
+    */
     printk(KERN_DEBUG "hello: Hello world!\n");
-    return 0;
+    return 0; // Non-zero return means that the module couldn't be loaded.
 }
 
 static void __exit hello_cleanup(void)
 {
+    /*
+        Unload
+    */
     printk(KERN_DEBUG "hello: Cleaning up module.\n");
 }
 
