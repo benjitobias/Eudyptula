554a68039d22093208ce973591370676871feb33
benjitobias@gmail.com <benjitobias@gmail.com>
2017-04-20 18:38:50 +0300

    Fixed coding style

--- 04/src/coding_style.c
+++ 04/src/coding_style.c
@@ -3,35 +3,31 @@
 #include <asm/delay.h>
 #include <linux/slab.h>
 
-int do_work( int * my_int, int retval ) {
+int do_work(int *my_int, int retval)
+{
 	int x;
-	int y=*my_int;
+	int y = *my_int;
 	int z;
-	
-	for(x=0;x< * my_int;++x) {
-		udelay(10);
-	}
 
-	if (y < 10 )
-		// That was a long sleep, tell userspace about it
+	for(x = 0; x < *my_int; ++x)
+		udelay(10);
+
+	if (y < 10)
 		printk("We slept a long time!");
 
 	z = x * y;
-
 	return z;
 }
 
-int
-my_init (void)
+int my_init (void)
 {
 	int x = 10;
 
 	x = do_work(&x, x);
-
 	return x;
 }
 
-void my_exit( void )
+void my_exit(void)
 {
 	return;
 }
