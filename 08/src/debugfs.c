b311676bf41d2556809078ff115ec08232bd20ef
benjitobias@gmail.com <benjitobias@gmail.com>
2018-09-15 13:48:59 +0300

    File now return eduyptula id

--- 08/src/debugfs.c
+++ 08/src/debugfs.c
@@ -2,35 +2,52 @@
 #include <linux/kernel.h> // included for KERN_DEBUG
 #include <linux/init.h> // included for __init and __exit macros
 #include <linux/debugfs.h> // included for debugfs
+#include <linux/fs.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("BENJI");
 MODULE_DESCRIPTION("Eudyptula 08 debugfs module");
 
-// Directory entry point will point to '/sys/kernel/debug/eudyptula'
-static struct dentry *dir = 0;
+#define EUD_ID "b9c2282a294c"
+#define ID_LEN sizeof(EUD_ID)
 
-// File 'sys/kernel/debug/eudyptula/id'
-static u32 id = 0;
+// Directory entry point will point to '/sys/kernel/debug/eudyptula'
+// File will point to '/sys/kernel/debug/eudyptula/id'
+static struct dentry *dir;
+static struct dentry *id;
+
+static char *id_buf = EUD_ID;
+
+int filevalue;
+
+static ssize_t dev_read(struct file *fp, char __user *user_buffer, size_t count, loff_t *position)
+{
+	return simple_read_from_buffer(user_buffer, count, position, id_buf, ID_LEN);
+}
+
+static const struct file_operations f_ops = {
+	.read = dev_read,
+};
 
 static int __init hello_init(void)
-{	
+{
 	struct dentry *junk;
+
 	printk(KERN_DEBUG "eudyptula 08: Loaded!\n");
-	
+
 	// create dir
 	dir = debugfs_create_dir("eudyptula", 0);
 	if (!dir) {
 		printk(KERN_ALERT, "eudyptula 08: failed to create /sys/kernel/debug/eudyptula\n");
 		return -1;
 	}
-	
-	junk = debugfs_create_u32("id", 0666, dir, &id);
+
+	junk = debugfs_create_file("id", 0666, dir, &filevalue, &f_ops);
 	if (!junk) {
 		printk(KERN_ALERT, "eudyptula 08: failed to create /sys/kernel/debug/eudyptula/id\n");
 		return -2;
 	}
-	
+
 	return 0; // Non-zero return means that the module couldn't be loaded.
 }
 
